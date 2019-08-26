e0a343143ab62e01f634d4b66a12a164d34dd802
MESURF88 <hill.kevin@earthlink.net>
2019-08-26 07:21:39 -0400

    Add files via upload
    Initial Upload with Visual Studio Solutions

---
+++ Part1/linkedList.c
@@ -0,0 +1,387 @@
+/***********************************************************
+* Author: Kevin Hil
+* Email: hillkev@oregonstate.edu
+* Date Created: 07/25/2019
+* Filename: linkedList.c
+*
+* Overview:
+*   This program is a linked list implementation of the deque
+*	(as worked on in Worksheet 19) and bag (as worked on in
+*	Worksheet 22) ADTs.
+*	The deque ADT allows for the following behavior:
+*		- adding a new link to the front/back
+*		- getting the value of the front/back links
+*		- removing the front/back link
+*	The bag ADT allows for the following behavior:
+*		- adding a new link
+*		- checking if a link exists with a given value
+*		- removing a link  with a given value if it exists
+*	Both allow for:
+*		- checking if empty
+*		- printing the values of all of the links
+*
+*	Note that both implementations utilize a linked list with
+*	both a front and back sentinel and double links (links with
+*	next and prev pointers).
+************************************************************/
+#include "linkedList.h"
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef FORMAT_SPECIFIER
+#define FORMAT_SPECIFIER "%d"
+#endif
+
+// Double link
+struct Link
+{
+	TYPE value;
+	struct Link* next;
+	struct Link* prev;
+};
+
+// Double linked list with front and back sentinels
+struct LinkedList
+{
+	struct Link* frontSentinel;
+	struct Link* backSentinel;
+	int size;
+};
+
+/**
+  	Allocates the list's sentinel and sets the size to 0.
+  	The sentinels' next and prev should point to eachother or NULL
+  	as appropriate.
+	param: 	list 	struct LinkedList ptr
+	pre: 	list is not null
+	post: 	list front and back sentinel not null
+			front sentinel next points to back
+			front sentinel prev points to null
+			back sentinel prev points to front
+			back sentinel next points to null
+			list size is 0
+ */
+static void init(struct LinkedList* list) {
+	assert(list != 0);
+	list->frontSentinel = (struct Link*) malloc(sizeof(struct Link));
+	assert(list->frontSentinel != 0);
+	list->backSentinel = (struct Link*) malloc(sizeof(struct Link));
+	assert(list->backSentinel != 0);
+	
+	list->frontSentinel->next = list->backSentinel;
+	list->frontSentinel->value = 0;
+	list->backSentinel->prev = list->frontSentinel;
+	list->backSentinel->value = 0;
+	list->size = 0;
+}
+
+/**
+ 	Adds a new link with the given value before the given link and
+	increments the list's size.
+ 	param: 	list 	struct LinkedList ptr
+ 	param:	link 	struct Link ptr
+ 	param: 	TYPE
+	pre: 	list and link are not null
+	post: 	newLink is not null
+			newLink w/ given value is added before param link
+			list size is incremented by 1
+ */
+static void addLinkBefore(struct LinkedList* list, struct Link* link, TYPE value)
+{
+	assert(list != 0);
+	assert(link != 0);
+	struct Link* newLink = (struct Link*) malloc(sizeof(struct Link));
+	assert(newLink != 0);
+	
+	newLink->value = value;
+	newLink->next = link;
+	newLink->prev = link->prev;
+	struct Link* temp = link->prev;
+	temp->next = newLink;
+	link->prev = newLink;
+	list->size++;
+}
+
+/**
+	Removes the given link from the list and
+	decrements the list's size.
+	param: 	list 	struct LinkedList ptr
+ 	param:	link 	struct Link ptr
+	pre: 	list and link are not null
+	post: 	param link is removed from param list
+			memory allocated to link is freed
+			list size is decremented by 1
+ */
+static void removeLink(struct LinkedList* list, struct Link* link)
+{
+	assert(list != 0);
+	assert(link != 0);
+	struct Link* link_n = link->next;
+	assert(link_n != 0);
+	struct Link* link_p = link->prev;
+	assert(link_p != 0);
+	link_p->next = link->next;
+	link_n->prev = link->prev;
+	list->size--;
+	free(link);
+}
+
+/**
+	Allocates and initializes a list.
+	pre: 	none
+	post: 	memory allocated for new struct LinkedList ptr
+			list init (call to init func)
+	return: list
+ */
+struct LinkedList* linkedListCreate()
+{
+	struct LinkedList* list = malloc(sizeof(struct LinkedList));
+	init(list);
+	return list;
+}
+
+/**
+	Deallocates every link in the list including the sentinels,
+	and frees the list itself.
+	param:	list 	struct LinkedList ptr
+	pre: 	list is not null
+	post: 	memory allocated to each link is freed
+			" " front and back sentinel " "
+			" " list " "
+ */
+void linkedListDestroy(struct LinkedList* list)
+{
+	assert(list != NULL);
+	while (!linkedListIsEmpty(list)) {
+		linkedListRemoveFront(list);
+	}
+	free(list->frontSentinel);
+	free(list->backSentinel);
+	free(list);
+	list = NULL;
+}
+
+/**
+	Adds a new link with the given value to the front of the deque.
+	param: 	deque 	struct LinkedList ptr
+	param: 	value 	TYPE
+	pre: 	deque is not null
+	post: 	link is created w/ param value stored before current first link
+			(call to addLinkBefore)
+ */
+void linkedListAddFront(struct LinkedList* deque, TYPE value)
+{
+	assert(deque != 0);
+	struct Link* newLink = (struct Link*) malloc(sizeof(struct Link));
+	assert(newLink != 0);
+	newLink->value = value;
+	struct Link* temp = deque->frontSentinel->next;
+	newLink->next = temp;
+	newLink->prev = deque->frontSentinel;
+
+	deque->frontSentinel->next=newLink;
+	temp->prev = newLink;
+	
+	deque->size++;
+	
+}
+
+/**
+	Adds a new link with the given value to the back of the deque.
+	param: 	deque 	struct LinkedList ptr
+	param: 	value 	TYPE
+	pre: 	deque is not null
+	post: 	link is created with given value before last link (sentinel)
+			(call to addLinkBefore)
+ */
+void linkedListAddBack(struct LinkedList* deque, TYPE value)
+{
+	assert(deque != 0);
+	struct Link* newLink = (struct Link*) malloc(sizeof(struct Link));
+	assert(newLink != 0);
+	newLink->value = value;
+	struct Link* temp = deque->backSentinel->prev;
+	newLink->next = deque->backSentinel;
+	newLink->prev = temp;
+	
+	deque->backSentinel->prev = newLink;
+	temp->next = newLink;
+
+	deque->size++;
+	
+}
+
+/**
+	Returns the value of the link at the front of the deque.
+	param: 	deque 	struct LinkedList ptr
+	pre:	deque is not null
+	pre:	deque is not empty
+	post:	none
+	ret:	first link's value
+ */
+TYPE linkedListFront(struct LinkedList* deque)
+{
+	assert(deque != 0);
+	assert(!linkedListIsEmpty(deque));
+	return deque->frontSentinel->next->value;
+}
+
+/**
+	Returns the value of the link at the back of the deque.
+	param: 	deque 	struct LinkedList ptr
+	pre:	deque is not null
+	pre:	deque is not empty
+	post:	none
+	ret:	last link's value
+ */
+TYPE linkedListBack(struct LinkedList* deque)
+{
+	assert(deque != 0);
+	assert(!linkedListIsEmpty(deque));
+	return deque->backSentinel->prev->value;
+}
+
+/**
+	Removes the link at the front of the deque.
+	param: 	deque 	struct LinkedList ptr
+	pre:	deque is not null
+	pre:	deque is not empty
+	post:	first link is removed and freed (call to removeLink)
+ */
+void linkedListRemoveFront(struct LinkedList* deque)
+{
+	assert(deque != 0);
+	assert(!linkedListIsEmpty(deque));
+	removeLink(deque, deque->frontSentinel->next);
+	deque->size--;
+}
+
+/**
+	Removes the link at the back of the deque.
+	param: 	deque 	struct LinkedList ptr
+	pre:	deque is not null
+	pre:	deque is not empty
+	post:	last link is removed and freed (call to removeLink)
+ */
+void linkedListRemoveBack(struct LinkedList* deque)
+{
+	assert(deque != 0);
+	assert(!linkedListIsEmpty(deque));
+	removeLink(deque, deque->backSentinel->prev);
+	deque->size--;
+}
+
+/**
+	Returns 1 if the deque is empty and 0 otherwise.
+	param:	deque	struct LinkedList ptr
+	pre:	deque is not null
+	post:	none
+	ret:	1 if its size is 0 (empty), otherwise 0 (not empty)
+ */
+int linkedListIsEmpty(struct LinkedList* deque)
+{
+	assert(deque != 0);
+	return deque->size == 0;
+}
+
+/**
+	Prints the values of the links in the deque from front to back.
+	param:	deque	struct LinkedList ptr
+	pre:	deque is not null
+	post:	none
+	ret:	outputs to the console the values of the links from front
+			to back; if empty, prints msg that is empty
+ */
+void linkedListPrint(struct LinkedList* deque)
+{
+	assert(deque != 0);
+	if (linkedListIsEmpty(deque) == 1) {
+		printf("%s", "The list is empty.");
+	}
+	else {
+		struct Link* temp = deque->frontSentinel;
+		while (temp->next!=deque->backSentinel) {
+			printf(FORMAT_SPECIFIER, temp->next->value);
+			temp = temp->next;
+		}
+	}
+	
+}
+
+/**
+	Adds a link with the given value to the bag.
+	param:	bag		struct LinkedList ptr
+	param: 	value 	TYPE
+	pre: 	bag is not null
+	post: 	link is created w/ given value before current first link
+			(call to addLinkBefore)
+			Note that bag doesn't specify where new link should be added;
+			can be anywhere in bag according to its ADT.
+ */
+void linkedListAdd(struct LinkedList* bag, TYPE value)
+{
+	assert(bag != 0);
+	addLinkBefore(bag, bag->frontSentinel->next, value);
+	bag->size++;
+
+}
+
+/**
+	Returns 1 if a link with the value is in the bag and 0 otherwise.
+	param:	bag		struct LinkedList ptr
+	param: 	value 	TYPE
+	pre: 	bag is not null
+	post:	none
+	ret:	1 if link with given value found; otherwise, 0
+ */
+int linkedListContains(struct LinkedList* bag, TYPE value)
+{
+	assert(bag != 0);
+	if (linkedListIsEmpty(bag) == 1) {
+		printf("%s", "The list is empty.");
+		return 0;
+	}
+	else {
+		struct Link* temp = bag->frontSentinel;
+		while (temp->next != bag->backSentinel) {
+			if (value == temp->next->value) {
+				return 1;
+			}
+		}
+		return 0;
+	}
+}
+
+/**
+	Removes the first occurrence of a link with the given value.
+	param:	bag		struct LinkedList ptr
+	param: 	value 	TYPE
+	pre: 	bag is not null
+	post:	if link with given value found, link is removed
+			(call to removeLink)
+ */
+void linkedListRemove(struct LinkedList* bag, TYPE value)
+{
+	assert(bag != 0);
+	int removals = 0;
+	struct Link *pointing = malloc(sizeof(struct Link));
+	struct Link *point_n = malloc(sizeof(struct Link));
+	struct Link *point_p = malloc(sizeof(struct Link));
+	
+	pointing = bag->frontSentinel->next;
+	while (pointing->next != bag->backSentinel){
+		
+		if (pointing->value == value && removals == 0) {
+			point_n = pointing->next;
+			point_p = pointing->prev;
+			point_p->next = pointing->next;
+			point_n->prev = pointing->prev;
+			removeLink(bag, pointing);
+			removals++;
+			bag->size--;
+			return;
+		}
+		pointing = pointing->next;
+	}
+}
---
+++ Part2/circularList.c
@@ -0,0 +1,325 @@
+/***********************************************************
+* Author: Kevin Hill
+* Email: hillkev@oregonstate.edu
+* Date Created: 07/25/2019
+* Filename: circularList.c
+*
+* Overview:
+*   This program is a circular doubly linked list implementation
+*	of a deque with a front sentinel.
+*	It allows for the following behavior:
+*		- adding a new link to the front/back
+*		- getting the value of the front/back links
+*		- removing the front/back link
+*		- checking if the deque is empty
+*		- printing the values of all the links
+*		- reversing the order of the links
+*
+*	Note that this implementation uses double links (links with
+*	next and prev pointers) and that given that it is a circular
+*	linked deque the last link points to the sentinel and the first
+*	link points to the Sentinel -- instead of null.
+************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "circularList.h"
+
+#ifndef FORMAT_SPECIFIER
+#define FORMAT_SPECIFIER "%g"
+#endif
+
+// Double link
+struct Link
+{
+	TYPE value;
+	struct Link * next;
+	struct Link * prev;
+};
+
+struct CircularList
+{
+	int size;
+	struct Link* sentinel;
+};
+
+/**
+  	Allocates the deque's sentinel and sets the size to 0.
+  	The sentinel's next and prev should point to the sentinel itself.
+ 	param: 	deque 	struct CircularList ptr
+	pre: 	deque is not null
+	post: 	deque sentinel not null
+			sentinel next points to sentinel
+			sentinel prev points to sentinel
+			deque size is 0
+ */
+static void init(struct CircularList* deque)
+{
+	assert(deque != 0);
+	deque->sentinel = (struct Link*) malloc(sizeof(struct Link));
+	assert(deque->sentinel != 0);
+
+	deque->sentinel->next = deque->sentinel;
+	deque->sentinel->prev = deque->sentinel;
+	deque->sentinel->value = 0;
+	deque->size = 0;
+}
+
+/**
+	Creates a link with the given value and NULL next and prev pointers.
+	param: 	value 	TYPE
+	pre: 	none
+	post: 	newLink is not null
+			newLink value init to value
+			newLink next and prev init to NULL
+ */
+static struct Link* createLink(TYPE value)
+{
+	struct Link* newLink = malloc(sizeof(struct Link));
+	assert(newLink != 0);
+	newLink->value = value;
+	newLink->next = NULL;
+	newLink->prev = NULL;
+}
+
+/**
+	Adds a new link with the given value after the given link and
+	increments the deque's size.
+	param: 	deque 	struct CircularList ptr
+ 	param:	link 	struct Link ptr
+ 	param: 	TYPE
+	pre: 	deque and link are not null
+	post: 	newLink is not null
+			newLink w/ given value is added after param link
+			deque size is incremented by 1
+ */
+static void addLinkAfter(struct CircularList* deque, struct Link* link, TYPE value)
+{
+	assert(deque != 0);
+	assert(link != 0);
+	struct Link* newLink = malloc(sizeof(struct Link));
+	assert(newLink != 0);
+
+	newLink->value = value;
+	newLink->prev = link;
+	newLink->next = link->next;
+	struct Link* temp = link->next;
+	temp->prev = newLink;
+	link->next = newLink;
+
+	deque->size++;
+}
+
+/**
+	Removes the given link from the deque and decrements the deque's size.
+	param: 	deque 	struct CircularList ptr
+ 	param:	link 	struct Link ptr
+	pre: 	deque and link are not null
+	post: 	param link is removed from param deque
+			memory allocated to link is freed
+			deque size is decremented by 1
+ */
+static void removeLink(struct CircularList* deque, struct Link* link)
+{
+	assert(deque != 0);
+	assert(link != 0);
+	struct Link* link_n = link->next;
+	assert(link_n != 0);
+	struct Link* link_p = link->prev;
+	assert(link_p != 0);
+	link_p->next = link->next;
+	link_n->prev = link->prev;
+	deque->size--;
+	free(link);
+}
+
+/*
+	Allocates and initializes a deque.
+	pre: 	none
+	post: 	memory allocated for new struct CircularList ptr
+			deque init (call to init func)
+	return: deque
+ */
+struct CircularList* circularListCreate()
+{
+	struct CircularList* deque = malloc(sizeof(struct CircularList));
+	init(deque);
+	return deque;
+}
+
+/**
+	Deallocates every link in the deque and frees the deque pointer.
+	pre: 	deque is not null
+	post: 	memory allocated to each link is freed
+			" " sentinel " "
+			" " deque " "
+ */
+void circularListDestroy(struct CircularList* deque)
+{
+	assert(deque != NULL);
+	while (!circularListIsEmpty(deque)) {
+		circularListRemoveFront(deque);
+	}
+	free(deque->sentinel);
+	free(deque);
+	deque = NULL;
+}
+
+/**
+	Adds a new link with the given value to the front of the deque.
+	param:	deque 	struct CircularList ptr
+	param: 	value 	TYPE
+	pre: 	deque is not null
+	post: 	link is created w/ given value before current first link
+			(call to addLinkAfter)
+ */
+void circularListAddFront(struct CircularList* deque, TYPE value)
+{
+	assert(deque != 0);
+	addLinkAfter(deque, deque->sentinel, value);
+}
+
+/**
+	Adds a new link with the given value to the back of the deque.
+	param: 	deque 	struct CircularList ptr
+	param: 	value 	TYPE
+	pre: 	deque is not null
+	post: 	link is created w/ given value after the current last link
+			(call to addLinkAfter)
+ */
+void circularListAddBack(struct CircularList* deque, TYPE value)
+{
+	assert(deque != 0);
+	//traverse list until last element is reached to add after that element
+	struct Link* temp = deque->sentinel;
+	while (temp->next != deque->sentinel) {
+		temp = temp->next;
+	}
+	addLinkAfter(deque, temp, value);
+}
+
+/**
+	Returns the value of the link at the front of the deque.
+	param: 	deque 	struct CircularList ptr
+	pre:	deque is not null
+	pre:	deque is not empty
+	post:	none
+	ret:	first link's value
+ */
+TYPE circularListFront(struct CircularList* deque)
+{
+	assert(deque!=0);
+	assert(!circularListIsEmpty(deque));
+	return deque->sentinel->next->value;
+}
+
+/**
+  	Returns the value of the link at the back of the deque.
+	param: 	deque 	struct CircularList ptr
+	pre:	deque is not null
+	pre:	deque is not empty
+	post:	none
+	ret:	last link's value
+ */
+TYPE circularListBack(struct CircularList* deque)
+{
+	assert(deque != 0);
+	assert(!circularListIsEmpty(deque));
+	return deque->sentinel->prev->value;
+}
+
+/**
+	Removes the link at the front of the deque.
+	param: 	deque 	struct CircularList ptr
+	pre:	deque is not null
+	pre:	deque is not empty
+	post:	first link is removed and freed (call to removeLink)
+ */
+void circularListRemoveFront(struct CircularList* deque)
+{
+	assert(deque != 0);
+	assert(!circularListIsEmpty(deque));
+	removeLink(deque, deque->sentinel->next);
+
+}
+
+/**
+	Removes the link at the back of the deque.
+	param: 	deque 	struct CircularList ptr
+	pre:	deque is not null
+	pre:	deque is not empty
+	post:	last link is removed and freed (call to removeLink)
+ */
+void circularListRemoveBack(struct CircularList* deque)
+{
+	assert(deque != 0);
+	assert(!circularListIsEmpty(deque));
+	removeLink(deque, deque->sentinel->prev);
+
+}
+
+/**
+	Returns 1 if the deque is empty and 0 otherwise.
+	param:	deque	struct CircularList ptr
+	pre:	deque is not null
+	post:	none
+	ret:	1 if its size is 0 (empty), otherwise 0 (not empty)
+ */
+int circularListIsEmpty(struct CircularList* deque)
+{
+	assert(deque != 0);
+	return deque->size == 0;
+}
+
+/**
+	Prints the values of the links in the deque from front to back.
+	param:	deque	struct CircularList ptr
+	pre:	deque is not null
+	post:	none
+	ret:	outputs to the console the values of the links from front
+			to back; if empty, prints msg that is empty
+ */
+void circularListPrint(struct CircularList* deque)
+{
+	assert(deque != 0);
+	if (circularListIsEmpty(deque) == 1) {
+		printf("%s", "The list is empty.");
+	}
+	else {
+		struct Link* temp = deque->sentinel;
+		while (temp->next != deque->sentinel) {
+			printf(FORMAT_SPECIFIER, temp->next->value);
+			temp = temp->next;
+		}
+	}
+}
+
+/**
+	Reverses the deque in place without allocating any new memory.
+	The process works as follows: current starts pointing to sentinel;
+	tmp points to current's next, current's next points to current's prev,
+	current's prev is assigned to tmp and current points to current's next
+	(which points to current's prev), so you proceed stepping back through
+	the deque, assigning current's next to current's prev, until current
+	points to the sentinel then you know the each link has been looked at
+	and the link order reversed.
+	param: 	deque 	struct CircularList ptr
+	pre:	deque is not null
+	pre:	deque is not empty
+	post:	order of deque links is reversed
+ */
+void circularListReverse(struct CircularList* deque)
+{
+	assert(deque != 0);
+	assert(!circularListIsEmpty(deque));
+	struct Link *current = deque->sentinel;
+	
+	do
+	{//re-initialize pointer each loop
+		struct Link* temp = current->prev;
+		current->prev = current->next;
+		current->next = temp;
+		current = current->prev;
+	} while (current != deque->sentinel);
+	//checks conditional after first loop to avoid exiting when current is set to sentinel
+}
